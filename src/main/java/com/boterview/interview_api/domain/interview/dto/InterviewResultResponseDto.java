0ad2267f2d7f5a05ef6ea8a1bbc81a54f5450a69
jeonggiju <halogiju123@gmailc.om>
2026-02-10 11:10:55 +0900

    add: interview 결과 dto

---
+++ src/main/java/com/boterview/interview_api/domain/interview/dto/InterviewResultResponseDto.java
@@ -0,0 +1,44 @@
+package com.boterview.interview_api.domain.interview.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class InterviewResultResponseDto {
+
+    private String interviewId;
+    private String userId;
+    private Long progressTime;
+    private String feedback;
+    private LocalDateTime createdAt;
+    private List<QuestionDto> interviewQuestion;
+    private List<ScoreDto> scores;
+
+    @Getter
+    @Builder
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class QuestionDto {
+        private Long questionId;
+        private String question;
+        private String answer;
+        private Long intervalTime;
+    }
+
+    @Getter
+    @Builder
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class ScoreDto {
+        private String scoreId;
+        private String scoreType;
+        private Integer score;
+        private String feedback;
+    }
+}
